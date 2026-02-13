97ac8fbb7a4e0658c1ef86cdefad151478b746dd
priamahal <0differenthang@gmail.com>
2026-02-13 22:12:14 +0700

    Leap and Treesitter
    fix: Change leap url to codeberg and Treesitter update to remove the
    event

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,63 +1,64 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" },
+	lazy = false,
 	build = ":TSUpdate",
+	priority = 1000,
+
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"nvim-treesitter/nvim-treesitter-context",
 		"windwp/nvim-ts-autotag",
 	},
-	config = function()
-		local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = { enable = true },
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-					},
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"css",
+			"html",
+			"javascript",
+			"typescript",
+			"tsx",
+			"json",
+			"lua",
+			"python",
+			"yaml",
+			"markdown",
+			"markdown_inline",
+			"vim",
+			"vimdoc",
+			"sql",
+			"dockerfile",
+			"gitignore",
+			"regex",
+			"toml",
+			"cmake",
+			"make",
+			"nix",
+			"graphql",
+			"query",
+		},
+
+		auto_install = true,
+
+		highlight = { enable = true },
+		indent = { enable = true },
+		autotag = { enable = true },
+
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
 				},
 			},
-			ensure_installed = {
-				"json",
-				"python",
-				"javascript",
-				"query",
-				"typescript",
-				"tsx",
-				"php",
-				"yaml",
-				"html",
-				"css",
-				"scss",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"lua",
-				"vim",
-				"vimdoc",
-				"c",
-				"dockerfile",
-				"gitignore",
-				"astro",
-				"toml",
-				"ini",
-				"kdl",
-				"make",
-				"cmake",
-				"sql",
-				"regex",
-				"graphql",
-				"nix",
-			},
-			auto_install = true,
-		})
+		},
+	},
+
+	config = function(_, opts)
+		require("nvim-treesitter").setup(opts)
 
 		local ok, context = pcall(require, "treesitter-context")
 		if ok then
